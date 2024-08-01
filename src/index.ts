9e25a428a3549f0e56b81e50ab7451228611b22c
odm275 <pomejia@gmail.com>
2024-08-01 17:50:22 -0500

    feat: typesafety for route params set up

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import Fastify, { FastifyInstance } from "fastify";
 import env from "@fastify/env";
 import { config } from "../config";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { Static, Type } from "@sinclair/typebox";
 
-declare module "Fastify" {
+declare module "fastify" {
   interface FastifyInstance {
     config: {
-      // this should be same as the confKey in options
-      // specify your typing here
+      PORT: number;
       NASA_NEOW_API_KEY: string;
+      REDIS_URL: string;
     };
   }
 }
@@ -24,6 +26,10 @@ const options = {
       NASA_NEOW_API_KEY: {
         type: "string",
       },
+      REDIS_URL: {
+        type: "string",
+        default: "redis://localhost:6379",
+      },
     },
   },
   dotenv: true, // This will load .env file
@@ -35,23 +41,40 @@ const fastify = Fastify({
       target: "pino-pretty",
     },
   },
+}).withTypeProvider<TypeBoxTypeProvider>();
+
+const QueryStringSchema = Type.Object({
+  start_date: Type.String(),
+  end_date: Type.String(),
 });
+type QueryStringType = Static<typeof QueryStringSchema>;
 
 async function apiRoutes(fastify: FastifyInstance) {
   fastify.get("/", (req, res) => {
     return { message: "Hello World" };
   });
-  fastify.get("/neow", async (req, res) => {
-    const searchParams = new URLSearchParams(config.neoWUrl);
-    searchParams.append("api_key", fastify.config.NASA_NEOW_API_KEY);
-    searchParams.append("start_date", "2024-08-01");
-    searchParams.append("end_date", "2024-08-01");
 
-    const url = `${config.neoWUrl}?${searchParams.toString()}`;
-    const resp = await fetch(url);
-    const data = await resp.json();
-    return data;
-  });
+  fastify.get<{ Querystring: QueryStringType }>(
+    "/neow",
+    {
+      schema: {
+        querystring: QueryStringSchema,
+      },
+    },
+    async (req, res) => {
+      const { start_date } = req.query;
+
+      const searchParams = new URLSearchParams(config.neoWUrl);
+      searchParams.append("api_key", fastify.config.NASA_NEOW_API_KEY);
+      searchParams.append("start_date", "2024-08-01");
+      searchParams.append("end_date", "2024-08-01");
+
+      const url = `${config.neoWUrl}?${searchParams.toString()}`;
+      const resp = await fetch(url);
+      const data = await resp.json();
+      return data;
+    }
+  );
 }
 
 async function main() {
---
+++ src/schemas/routes.ts
@@ -0,0 +1,16 @@
+// const neowSchema = {
+//   type: "object",
+//   additionalProperties: false,
+//   properties: {
+//     start_date: { type: "string" },
+//     end_date: { type: "string" },
+//   },
+//   required: ["start_date"],
+// } as const;
+
+// export const sharedSchema = {
+//   $id: "shared-schema",
+//   definitions: {
+//     neow: neowSchema,
+//   },
+// } as const;
